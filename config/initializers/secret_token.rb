a36113e8379ea36efcb9bf3f7fc4350bc3b03eec
ben2pop <r.bendenoun@gmail.com>
2013-06-30 14:44:50 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covoit::Application.config.secret_token = 'c2f4075ab7c13587a5b8ec5d70b3d6d06af229c60f9706e63a6c5b8dc13a7f2f527459ed363bb1b91568f257c2e3d32424cd3af6543b744d14663d29dcfcd045'
